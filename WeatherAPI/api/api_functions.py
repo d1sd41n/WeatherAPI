dc81c8a425929f6adf117471fbf873aca9b4d01e
Daniel Pérez <1498@holbertonschool.com>
2021-07-25 22:39:36 -0500

    unit test

--- WeatherAPI/api/api_functions.py
+++ WeatherAPI/api/api_functions.py
@@ -3,7 +3,17 @@ import requests
 
 
 def request_weather_data(city, country):
+    """[requests the openweathermap api, and
+    brings data about the weather of a city]
 
+    Args:
+        city ([str]): [name of a city]
+        country ([str]): [country code]
+
+    Returns:
+        [tuple]: [in position 0 the status code
+                  in position 1 weather data dict]
+    """
     base_api_url = 'http://api.openweathermap.org'
     api_url = f'/data/2.5/weather?q={city},{country}' +\
         '&appid=1508a9a4840a5574c822d70ca2132032'
@@ -13,22 +23,53 @@ def request_weather_data(city, country):
 
 
 def kelvin_2_celsius(kelvin):
+    """[convert kelvin units to Celsius]
+
+    Args:
+        kelvin ([float]): [kelvin units]
+
+    Returns:
+        [float]: [Celsius units]
+    """
     celsius = kelvin - 273.15
     return float("{:.2f}".format(celsius))
 
 
 def kelvin_2_fahrenheit(kelvin):
+    """[convert kelvin units to farenheit]
+
+    Args:
+        kelvin ([float]): [kelvin units]
+
+    Returns:
+        [float]: [farenheit units]
+    """
     fahren = (1.8 * kelvin) - 459.67
     return float("{:.2f}".format(fahren))
 
 
 def timestamp_2_time(timestamp):
+    """[converts a timestamp to a time string]
+
+    Args:
+        timestamp ([int]): [timestamp unix]
+
+    Returns:
+        [str]: [time hour:minutes]
+    """
     dt_object = datetime.fromtimestamp(timestamp)
     return dt_object.strftime("%H:%M")
 
 
 def process_data(data):
+    """[changes the format of the data obtained from openweathermap api]
 
+    Args:
+        data ([dict]): [data from openweathermap]
+
+    Returns:
+        [dict]: [Processed data]
+    """
     res_data = {}
 
     res_data["location_name"] = f"{data['name']}, {data['sys']['country']}"
@@ -74,7 +115,17 @@ def process_data(data):
 
 
 def get_weather_data(city, country):
+    """[request the openweathermap api and
+    return the weather data in a new format]
 
+    Args:
+        city ([str]): [name of a city]
+        country ([str]): [country code]
+
+    Returns:
+        [tuple]: [in position 0 the status code
+                  in position 1 weather data dict]
+    """
     status, data = request_weather_data(city, country)
 
     if status != 200:
--- WeatherAPI/api/tests.py
+++ WeatherAPI/api/tests.py
@@ -1,3 +1,146 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework.test import APIClient
 
-# Create your tests here.
+from .api_functions import (request_weather_data, kelvin_2_celsius,
+                            kelvin_2_fahrenheit, timestamp_2_time,
+                            process_data, get_weather_data)
+
+class TestApi(APITestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        print("\n" + "#" * 20)
+        print("Testing Api")
+        print("#" * 20)
+
+    def test_request_api(self):
+
+        print("\ntest_request_api")
+        url = '/weather?country=co&city=medellin'
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+
+        res_data = response.data
+        self.assertTrue('location_name' in res_data)
+        self.assertTrue('temperature_c' in res_data)
+        self.assertTrue('temperature_f' in res_data)
+        self.assertTrue('wind' in res_data)
+        self.assertTrue('cloudiness' in res_data)
+        self.assertTrue('pressure' in res_data)
+        self.assertTrue('humidity' in res_data)
+        self.assertTrue('sunrise' in res_data)
+        self.assertTrue('sunset' in res_data)
+        self.assertTrue('geo_coordinates' in res_data)
+        self.assertTrue('requested_time' in res_data)
+        self.assertTrue('forecast' in res_data)
+
+    def test_errors_api(self):
+        print("test_errors_api")
+
+        # capital letter country
+        url = '/weather?country=CO&city=medellin'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+        res_message = 'the country parameter cannot have uppercase letters'
+        self.assertEqual(response.data["error"], res_message)
+
+        # no city
+        url = '/weather?country=co'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+        res_message = 'the city parameter is required'
+        self.assertEqual(response.data["error"], res_message)
+
+        # no country
+        url = '/weather?city=medellin'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+        res_message = 'the country parameter is required'
+        self.assertEqual(response.data["error"], res_message)
+
+        # numeric country
+        url = '/weather?country=12&city=medellin'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 400)
+        res_message = 'the country must be alphabetic'
+        self.assertEqual(response.data["error"], res_message)
+
+        # numeric city
+        url = '/weather?country=co&city=434'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 400)
+        res_message = 'the city must be alphabetic'
+        self.assertEqual(response.data["error"], res_message)
+
+
+class TestFunctions(APITestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        print("\n" + "#" * 20)
+        print("TestFunctions")
+        print("#" * 20)
+
+    def test_request_weather_data_f(self):
+        print("test_request_weather_data_f")
+        status, data = request_weather_data("bogota", "co")
+        self.assertEqual(status, 200)
+        self.assertEqual(type(data), type({}))
+
+    def test_kelvin_2_celsius_f(self):
+        print("test_kelvin_2_celsius_f")
+        cel = kelvin_2_celsius(120)
+        self.assertEqual(cel, -153.15)
+
+    def test_kelvin_2_fahrenheit_f(self):
+        print("test_kelvin_2_celsius_f")
+        far = kelvin_2_fahrenheit(500)
+        self.assertEqual(far, 440.33)
+
+    def test_timestamp_2_time_f(self):
+        print("test_timestamp_2_time_f")
+        timestamp = 1627269534
+        time = timestamp_2_time(timestamp)
+        self.assertEqual(time, "03:18")
+
+    def test_process_data_f(self):
+        print("test_process_data_f")
+        input_data = {'coord': {'lon': -75.5636, 'lat': 6.2518},
+                      'weather': [{'id': 803, 'main': 'Clouds',
+                                   'description': 'broken clouds', 'icon': '04n'}],
+                      'base': 'stations', 'main': {'temp': 291.61, 'feels_like': 292.1,
+                                                   'temp_min': 290.96, 'temp_max': 292.02, 'pressure': 1027,
+                                                   'humidity': 99}, 'visibility': 10000,
+                      'wind': {'speed': 1.03, 'deg': 0},
+                      'clouds': {'all': 75}, 'dt': 1627269927,
+                      'sys': {'type': 2, 'id': 2002201,
+                              'country': 'CO', 'sunrise': 1627210569, 'sunset': 1627255266},
+                      'timezone': -18000, 'id': 3674962, 'name': 'Medellín', 'cod': 200}
+        output_data = process_data(input_data)
+
+        self.assertEqual(output_data["location_name"], "Medellín, CO")
+        self.assertEqual(output_data["temperature_c"], "18.46 °C")
+        self.assertEqual(output_data["wind"], "1.03 m/s")
+        self.assertEqual(output_data["temperature_f"], "65.23 °F")
+        self.assertEqual(output_data["cloudiness"], "broken clouds")
+        self.assertEqual(output_data["humidity"], "99%")
+        self.assertEqual(output_data["sunrise"], "10:56")
+
+    def test_get_weather_data_f(self):
+
+        print("test_get_weather_data_f")
+        status, res_data = get_weather_data("medellin", "co")
+        self.assertEqual(status, 200)
+        self.assertTrue('location_name' in res_data)
+        self.assertTrue('temperature_c' in res_data)
+        self.assertTrue('temperature_f' in res_data)
+        self.assertTrue('wind' in res_data)
+        self.assertTrue('cloudiness' in res_data)
+        self.assertTrue('pressure' in res_data)
+        self.assertTrue('humidity' in res_data)
+        self.assertTrue('sunrise' in res_data)
+        self.assertTrue('sunset' in res_data)
+        self.assertTrue('geo_coordinates' in res_data)
+        self.assertTrue('requested_time' in res_data)
+        self.assertTrue('forecast' in res_data)
